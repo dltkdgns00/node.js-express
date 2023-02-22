fd468eb55fcfa445564a53d1284abc5e35587c4b
dltkdgns00 <s20604@naver.com>
2023-02-22 14:21:10 +0900

    node.js-express study

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+const express = require('express');
+const app = express();
+var fs = require('fs');
+var template = require('./lib/template.js');
+
+app.get('/', function (request, response)
+{
+  fs.readdir('./data', function (error, filelist)
+  {
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    );
+    response.send(html);
+  });
+})
+
+app.get('/page/:pageId', function (request, response)
+{
+  return response.send(request.params);
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+/*
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -141,3 +169,4 @@ var app = http.createServer(function(request,response){
     }
 });
 app.listen(3000);
+*/
